ea34c7a561911a24de3674619cb984ffc410c42d
ywqzzy <592838129@qq.com>
2020-01-01 19:12:52 +0800

    refractor

---
+++ lexer.py
@@ -0,0 +1,68 @@
+from constants import *
+from position import Position
+from tokens import Token
+from error import IllegalCharError
+
+class Lexer:
+    def __init__(self, file_name, text):
+        self.text = text
+        self.file_name = file_name
+        self.pos = Position(-1, 0, -1, file_name, text)
+        self.current_char = None
+        self.advance()
+    
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+    
+    def make_tokens(self):
+        tokens = []
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+            else:
+                # return some error
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+
+        return tokens, None
+    
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+        
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else: 
+            return Token(TT_FLOAT, float(num_str))
---
+++ position.py
@@ -0,0 +1,19 @@
+class Position:
+    def __init__(self, idx, line_num, col_num, file_name, file_text):
+        self.file_name = file_name
+        self.file_text = file_text
+        self.idx = idx
+        self.line_num = line_num
+        self.col_num = col_num
+    
+    def advance(self, current_char):
+        self.idx += 1 
+        self.col_num += 1
+        if current_char == '\n':
+            self.line_num += 1
+            self.col_num = 0
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.line_num, self.col_num, self.file_name, self.file_text)
+        
